9e5cccda7be7bf6387dcf12397bc6b81ce721fc7
Luis <luisalon1s2o@gmail.com>
2024-10-18 23:21:13 -0600

    plantilla y modelo de la entidadConexión a la base de datos

---
+++ CapaEntidad/Motos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Motos
+    {
+        public int IdMotos {  get; set; }
+        public string CodigoChasis { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string  Motor {  get; set; }
+        public DateTime FechaCompra {  get; set; } = DateTime.Now;
+    }
+}
